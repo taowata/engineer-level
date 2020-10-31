0628abd0d8a421072dc69e67c3cdbfc9ea7e4069
taowata <momossy5@gmail.com>
2020-10-31 15:04:12 +0900

    API Service追加

--- app/src/main/java/io/github/taowata/engineerlevel/network/GitHubApiService.kt
+++ app/src/main/java/io/github/taowata/engineerlevel/network/GitHubApiService.kt
@@ -1,2 +1,24 @@
 package io.github.taowata.engineerlevel.network
 
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL = "https://api.github.com/"
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface GitHubApiService {
+    @GET("users/taowata")
+    fun getProperties():
+            Call<String>
+}
+
+object GitHubApi {
+    val retrofitService : GitHubApiService by lazy {
+        retrofit.create(GitHubApiService::class.java) }
+}
