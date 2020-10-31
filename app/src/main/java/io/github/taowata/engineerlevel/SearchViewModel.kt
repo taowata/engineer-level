3f0db500466c6975191cd751ebed2952a3851d47
taowata <momossy5@gmail.com>
2020-10-31 15:22:52 +0900

    add SearchViewModel

---
+++ app/src/main/java/io/github/taowata/engineerlevel/SearchViewModel.kt
@@ -0,0 +1,32 @@
+package io.github.taowata.engineerlevel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import io.github.taowata.engineerlevel.network.GitHubApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class SearchViewModel : ViewModel() {
+
+    private val _response = MutableLiveData<String>()
+
+    val response: LiveData<String>
+        get() = _response
+
+    private fun getGitHubUserProperties() {
+        GitHubApi.retrofitService.getProperties().enqueue(
+            object: Callback<String> {
+                override fun onResponse(call: Call<String>, response: Response<String>) {
+                    _response.value = response.body()
+                }
+
+                override fun onFailure(call: Call<String>, t: Throwable) {
+                    _response.value = "Failure: " + t.message
+                }
+            }
+        )
+    }
+
+}
