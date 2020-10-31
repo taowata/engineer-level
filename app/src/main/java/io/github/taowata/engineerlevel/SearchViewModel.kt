d9b7ca74dc1c10f3ea3d00927c4a57301e9b28e5
taowata <momossy5@gmail.com>
2020-10-31 16:55:30 +0900

    JSONをGitHubUserオブジェクトへパース

--- app/src/main/java/io/github/taowata/engineerlevel/SearchViewModel.kt
+++ app/src/main/java/io/github/taowata/engineerlevel/SearchViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import io.github.taowata.engineerlevel.network.GitHubApi
+import io.github.taowata.engineerlevel.network.GitHubUser
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -21,13 +22,13 @@ class SearchViewModel : ViewModel() {
     }
 
     private fun getGitHubUserProperties() {
-        GitHubApi.retrofitService.getProperties().enqueue(
-            object: Callback<String> {
-                override fun onResponse(call: Call<String>, response: Response<String>) {
-                    _response.value = response.body()
+        GitHubApi.retrofitService.getUser().enqueue(
+            object: Callback<GitHubUser> {
+                override fun onResponse(call: Call<GitHubUser>, response: Response<GitHubUser>) {
+                    _response.value = "Success: We got @${response.body()?.userName}'s information "
                 }
 
-                override fun onFailure(call: Call<String>, t: Throwable) {
+                override fun onFailure(call: Call<GitHubUser>, t: Throwable) {
                     _response.value = "Failure: " + t.message
                 }
             }
---
+++ app/src/main/java/io/github/taowata/engineerlevel/network/GitHubUser.kt
@@ -0,0 +1,9 @@
+package io.github.taowata.engineerlevel.network
+
+import com.squareup.moshi.Json
+
+data class GitHubUser(
+    @Json(name = "login") val userName: String,
+    val followers: Int,
+    val following: Int
+)
