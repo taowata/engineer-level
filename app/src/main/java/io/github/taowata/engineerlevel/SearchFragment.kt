99295a6cb403161afe1956c245ad464b2a7192f8
taowata <momossy5@gmail.com>
2020-10-31 15:32:23 +0900

    SearchFragmentの追加

---
+++ app/src/main/java/io/github/taowata/engineerlevel/SearchFragment.kt
@@ -0,0 +1,28 @@
+package io.github.taowata.engineerlevel
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+
+class SearchFragment : Fragment() {
+
+    private val viewModel: SearchViewModel by lazy {
+        ViewModelProvider(this).get(SearchViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = SearchFragment()
+    }
+}
