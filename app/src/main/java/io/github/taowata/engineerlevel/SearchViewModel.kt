c51aad9867a2cf0e33f462fe081aa256d981163a
Tao Watanabe <momossy5@gmail.com>
2020-11-07 21:00:57 +0900

    viewModelの初期化をコメント

--- app/src/main/java/io/github/taowata/engineerlevel/SearchViewModel.kt
+++ app/src/main/java/io/github/taowata/engineerlevel/SearchViewModel.kt
@@ -1,6 +1,5 @@
 package io.github.taowata.engineerlevel
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,9 +7,7 @@ import androidx.lifecycle.viewModelScope
 import io.github.taowata.engineerlevel.network.CommitList
 import io.github.taowata.engineerlevel.network.GitHubApi
 import io.github.taowata.engineerlevel.network.Repository
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import java.lang.Exception
 
 class SearchViewModel : ViewModel() {
@@ -21,9 +18,9 @@ class SearchViewModel : ViewModel() {
         get() = _response
 
     // viewModel初期化時に通信
-    init {
-        getGitHubUserProperties()
-    }
+//    init {
+//        getGitHubUserProperties()
+//    }
 
     private fun getGitHubUserProperties() {
         viewModelScope.launch {
@@ -33,13 +30,8 @@ class SearchViewModel : ViewModel() {
                 val repositories = GitHubApi.retrofitService.getRepositories(userName)
 
                 // コミット数の取得
-                var allCommitNumber = 0
+                val allCommitNumber = calculateTotalCommits(userName, repositories)
 
-//                for (repository in repositories) {
-//                    val repoName = repository.name
-//                    val commitList: CommitList = GitHubApi.retrofitService.getCommitList(userName, repoName)
-//                    allCommitNumber += commitList.commitList.sum()
-//                }
                 _response.value = "Success: @${gitHubUser.userName} has ${repositories.size} repositories and $allCommitNumber Commits"
             } catch (e: Exception) {
                 _response.value = "Failure: ${e.message}"
