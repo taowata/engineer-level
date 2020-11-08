7557ecb7a56c3e825fee93a37540882e48986a99
Tao Watanabe <momossy5@gmail.com>
2020-11-08 15:50:34 +0900

    コミット数、スター数、使用言語数を計算する

--- app/src/main/java/io/github/taowata/engineerlevel/SearchViewModel.kt
+++ app/src/main/java/io/github/taowata/engineerlevel/SearchViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.viewModelScope
 import io.github.taowata.engineerlevel.network.CommitList
 import io.github.taowata.engineerlevel.network.GitHubApi
 import io.github.taowata.engineerlevel.network.Repository
+import io.github.taowata.engineerlevel.network.StarGazer
 import kotlinx.coroutines.launch
 import java.lang.Exception
 
@@ -18,37 +19,63 @@ class SearchViewModel : ViewModel() {
         get() = _response
 
     // viewModel初期化時に通信
-//    init {
-//        getGitHubUserProperties()
-//    }
+    init {
+        getGitHubUserProperties()
+    }
 
     private fun getGitHubUserProperties() {
         viewModelScope.launch {
             try {
-                val userName = "taowata"
+                val userName = "Ossamoon"
                 val gitHubUser = GitHubApi.retrofitService.getUser(userName)
                 val repositories = GitHubApi.retrofitService.getRepositories(userName)
 
-                // コミット数の取得
-                val allCommitNumber = calculateTotalCommits(userName, repositories)
+                // Triple<Int, Int, MutableMap<String, Long>が返される
+                val (allCommitNumber, allStarNumber, languageMap) = calculateUserProperties(userName, repositories)
 
-                _response.value = "Success: @${gitHubUser.userName} has ${repositories.size} repositories and $allCommitNumber Commits"
+                _response.value =
+                    "Success: @${gitHubUser.userName} has ${repositories.size} repositories and $allCommitNumber Commits, $allStarNumber Stars." +
+                            "@${gitHubUser.userName}'s languages: $languageMap"
             } catch (e: Exception) {
                 _response.value = "Failure: ${e.message}"
             }
         }
     }
 
-    private suspend fun calculateTotalCommits(userName: String, repositories: List<Repository>): Int {
+    private suspend fun calculateUserProperties(
+        userName: String,
+        repositories: List<Repository>): Triple<Int, Int, MutableMap<String, Long>> {
         var allCommitNumber = 0
-
+        var allStarNumber = 0
+        val languageMap: MutableMap<String, Long> = mutableMapOf()
         for (repository in repositories) {
             val repoName = repository.name
+
+            // コミット数の計算
             val commitList: CommitList = GitHubApi.retrofitService.getCommitList(userName, repoName)
             allCommitNumber += commitList.commitList.sum()
+
+            // スター数の計算
+            val starGazerList: List<StarGazer> =
+                GitHubApi.retrofitService.getStarGazerList(userName, repoName)
+            allStarNumber += starGazerList.size
+
+            // 使用言語のByte数の計算
+            val tempMap: MutableMap<String, Long> =
+                GitHubApi.retrofitService.getLanguageBytes(userName, repoName)
+
+            for ((k, v) in tempMap) {
+                if (languageMap[k] == null) {
+                    languageMap += k to v
+                } else {
+                    val pastBytes: Long = languageMap[k] ?: 0L
+                    val addBytes: Long = tempMap[k] ?: 0L
+                    languageMap[k] = pastBytes + addBytes
+                }
+            }
+
         }
 
-        return allCommitNumber
+        return Triple(allCommitNumber, allStarNumber, languageMap)
     }
-
 }
