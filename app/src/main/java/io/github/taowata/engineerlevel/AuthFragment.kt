8e5d290771619303c5b5845fc4feb2718d5ac142
Tao Watanabe <momossy5@gmail.com>
2020-11-12 22:49:54 +0900

    Firebaseログインの実装

--- app/src/main/java/io/github/taowata/engineerlevel/AuthFragment.kt
+++ app/src/main/java/io/github/taowata/engineerlevel/AuthFragment.kt
@@ -1,15 +1,16 @@
 package io.github.taowata.engineerlevel
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import org.jsoup.Jsoup
+import androidx.navigation.findNavController
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 
 class AuthFragment : Fragment() {
 
@@ -17,17 +18,46 @@ class AuthFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
-            val document = Jsoup.connect("https://github.com/taowata").get()
-            Log.i("parse", document.select("h2.f4.text-normal.mb-2").last().text().split(" ").map { it.toString() }.first())
-        }
 
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+        )
 
-//        val text = document.select(".f4 text-normal mb-2").first().text()
-//        GitHubHomeHtmlParser.getContributionNumber("https://github.com/taowata")
-        // Inflate the layout for this fragment
+        // Create and launch sign-in intent
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN)
+
         return inflater.inflate(R.layout.fragment_auth, container, false)
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                val view = requireView()
+                view.findNavController().navigate(R.id.action_authFragment_to_searchFragment)
+
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+
+    companion object {
+
+        private const val RC_SIGN_IN = 123
+    }
 
 }
