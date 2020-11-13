0be9f0e8840dec095eeff18e44fe013a3363fb28
Tao Watanabe <momossy5@gmail.com>
2020-11-14 01:27:35 +0900

    DBに保存したデータにアクセス

--- app/src/main/java/io/github/taowata/engineerlevel/FavoriteListFragment.kt
+++ app/src/main/java/io/github/taowata/engineerlevel/FavoriteListFragment.kt
@@ -5,15 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import io.github.taowata.engineerlevel.databinding.FragmentFavoriteListBinding
 
 class FavoriteListFragment : Fragment() {
+
+    private lateinit var binding: FragmentFavoriteListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite_list, container, false)
+
+        binding = FragmentFavoriteListBinding.inflate(inflater)
+
+        val textView = binding.textView
+
+        val firebaseUser = FirebaseAuth.getInstance().currentUser
+        val db = FirebaseFirestore.getInstance()
+
+        db.collection("users").document(firebaseUser?.uid ?: "unknownUser")
+            .collection("engineers")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    val text = textView.text.toString() + document.data["name"]
+                    textView.text = text
+                }
+            }
+
+        return binding.root
     }
 
 }
