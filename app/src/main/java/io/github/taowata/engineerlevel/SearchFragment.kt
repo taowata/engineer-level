32c4b09c4397592b0588bc3a72db2aef0d225d04
Tao Watanabe <momossy5@gmail.com>
2020-11-13 22:33:36 +0900

    firestoreを実装

--- app/src/main/java/io/github/taowata/engineerlevel/SearchFragment.kt
+++ app/src/main/java/io/github/taowata/engineerlevel/SearchFragment.kt
@@ -1,6 +1,7 @@
 package io.github.taowata.engineerlevel
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.firebase.ui.auth.AuthUI
+import com.google.firebase.firestore.FirebaseFirestore
 import io.github.taowata.engineerlevel.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
@@ -46,6 +48,31 @@ class SearchFragment : Fragment() {
                 .signOut(requireContext())
                 .addOnCompleteListener {
                     view.findNavController().navigate(R.id.action_searchFragment_to_authFragment)
+                }
+
+        }
+
+        binding.floatingActionButton.setOnClickListener {
+            val db = FirebaseFirestore.getInstance()
+            val engineer = hashMapOf(
+                "name" to "taowata",
+                "contributions" to 923,
+                "followers" to 2,
+                "stars" to 1,
+                "languagesAndBytes" to hashMapOf(
+                    "JavaScript" to 2972783,
+                    "Kotlin" to 671871,
+                    "HTML" to 222565,
+                    "CSS" to 27053,
+                )
+            )
+            db.collection("engineers").document(engineer["name"].toString())
+                .set(engineer)
+                .addOnSuccessListener { documentReference ->
+                    Log.d("tag", "DocumentSnapshot added with ID: $documentReference")
+                }
+                .addOnFailureListener { e ->
+                    Log.w("tag", "Error adding document", e)
                 }
         }
 
