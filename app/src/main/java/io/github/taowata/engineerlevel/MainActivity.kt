717971d496c9a18e5274da8843bcbdf29d93ba03
Tao Watanabe <momossy5@gmail.com>
2020-11-13 22:45:05 +0900

    EditText外タップでキーボードを閉じる

--- app/src/main/java/io/github/taowata/engineerlevel/MainActivity.kt
+++ app/src/main/java/io/github/taowata/engineerlevel/MainActivity.kt
@@ -1,11 +1,23 @@
 package io.github.taowata.engineerlevel
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // EditText外をタップした時にキーボードを閉じる
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (currentFocus != null) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+        }
+        return super.dispatchTouchEvent(ev)
     }
 }
